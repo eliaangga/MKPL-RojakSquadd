5926a1c6502f20d501ed2f81e52dc1b60b107b5d
Pownyfals <pownyfals@gmail.com>
2023-06-05 19:26:08 +0700

    coba error

--- src/main/java/com/example/MvcReference/MvcReferenceApplication.java
+++ src/main/java/com/example/MvcReference/MvcReferenceApplication.java
@@ -8,7 +8,7 @@ public class MvcReferenceApplication {
 
 	// main app
 	public static void main(String[] args) {
-		SpringApplication.run(MvcReferenceApplication.class, args);
+		SpringApplicationn.run(MvcReferenceApplication.class, args);
 	}
 
 }
